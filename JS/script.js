0fd616a8962d1a3d5e0a7d28204b0ba91dda7c63
Morsolai <morsolai101@gmail.com>
2025-01-04 00:13:48 +0200

    Add close button to sidebar and update link behavior
    Added a close button to the sidebar for improved usability, with styles and behavior to hide the sidebar when clicked. Removed the "target" attribute from all links to ensure consistent navigation. Updated page footers to reflect the year 2025.

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,6 @@
 const menuButton = document.getElementById("menu-button");
 const sidebar = document.getElementById("sidebar");
+const closeButton = document.getElementById("close-button");
 const content = document.getElementById("content");
 
 document.addEventListener("click", (event) => {
@@ -12,4 +13,17 @@ document.addEventListener("click", (event) => {
 
 menuButton.addEventListener("click", () => {
     sidebar.classList.toggle("active");
+});
+
+// Додавання закриття через хрестик
+closeButton.addEventListener("click", () => {
+    sidebar.classList.remove("active");
+});
+
+// Знаходимо всі посилання на сторінці
+const links = document.querySelectorAll("a");
+
+// Перебираємо та видаляємо атрибут "target" для кожного посилання
+links.forEach(link => {
+    link.removeAttribute("target");
 });
